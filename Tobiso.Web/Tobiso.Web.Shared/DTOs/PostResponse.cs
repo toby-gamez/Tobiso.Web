66e79298df10416caecc0cfdb64ab60921ba96b1
caloriscz <caloris@caloris.cz>
2025-07-07 18:23:13 +0200

    Add initial migration setup and implement Post and Category entities with corresponding services and controller

---
+++ Tobiso.Web/Tobiso.Web.Shared/DTOs/PostResponse.cs
@@ -0,0 +1,13 @@
+﻿using Tobiso.Web.Domain.Entities;
+
+namespace Tobiso.Web.Shared.DTOs;
+
+public class PostResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string FilePath { get; set; } = string.Empty;
+    public DateTime? UpdatedAt { get; set; }
+    public int? CategoryId { get; set; }
+    public Category? Category { get; set; }
+}
