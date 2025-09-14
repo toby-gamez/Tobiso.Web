ff54cf483a7f9744546aa45f0c6363d06a67cdf1
toby-gamez <henematobik213@gmail.com>
2025-09-14 16:03:04 +0200

    Add authentication checks and improve layout components
    - Introduced authentication checks in multiple pages to ensure users are logged in before accessing content.
    - Updated layout components to include logout functionality.
    - Added a new configuration file for server settings.

--- Tobiso.Web.App.Admin/Program.cs
+++ Tobiso.Web.App.Admin/Program.cs
@@ -1,4 +1,3 @@
-using Tobiso.Web.Shared.Interfaces;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore;
@@ -8,10 +7,10 @@ using Serilog;
 using Tobiso.Api.Authentication;
 using Tobiso.Api.Infrastructure.Data;
 using Tobiso.Web.Api.Services;
-using Tobiso.Web.App.Authentication;
 using Tobiso.Web.App.Admin.Components;
+using Tobiso.Web.App.Authentication;
 using Tobiso.Web.App.Handlers;
-
+using Tobiso.Web.Shared.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,12 +21,14 @@ Log.Logger = new LoggerConfiguration()
 
 builder.Host.UseSerilog();
 
-
 var services = builder.Services;
 
 // Add services
 services.Configure<BasicAuthOptions>(builder.Configuration.GetSection("Auth:Basic"));
-services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["Api:BaseAddress"]) });
+services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration["Api:BaseAddress"]),
+});
 services.AddHttpContextAccessor();
 
 services.AddDbContext<TobisoDbContext>(options =>
@@ -35,8 +36,9 @@ services.AddDbContext<TobisoDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-services.AddAuthentication(BasicAuthConstants.Scheme).AddScheme<AuthenticationSchemeOptions, BasicAuthHandler>(
-        BasicAuthConstants.Scheme, null);
+services
+    .AddAuthentication(BasicAuthConstants.Scheme)
+    .AddScheme<AuthenticationSchemeOptions, BasicAuthHandler>(BasicAuthConstants.Scheme, null);
 
 services.AddAuthorization();
 
@@ -46,23 +48,24 @@ services.AddScoped<AuthenticationStateProvider, BasicAuthenticationStateProvider
 
 services.AddRazorComponents().AddInteractiveServerComponents();
 services.AddScoped<ICategoryService, CategoryService>();
-services.AddControllers()
+services
+    .AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+        options.JsonSerializerOptions.Converters.Add(
+            new System.Text.Json.Serialization.JsonStringEnumConverter()
+        );
     });
 services.AddEndpointsApiExplorer();
 
 services.AddScoped<IPostService, PostService>();
 
-
-
 services.AddSingleton<CredentialStore>();
 services.AddTransient<AuthenticationHeaderHandler>();
 services.AddTransient<HttpLoggingHandler>();
 
-
-services.AddRefitClient<ITobisoWebApi>()
+services
+    .AddRefitClient<ITobisoWebApi>()
     .ConfigureHttpClient(c =>
     {
         var baseAddress = builder.Configuration["Api:BaseAddress"];
@@ -75,40 +78,39 @@ services.AddRefitClient<ITobisoWebApi>()
     .AddHttpMessageHandler<AuthenticationHeaderHandler>()
     .AddHttpMessageHandler<HttpLoggingHandler>();
 
-
 services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "Tobiso API",
-        Version = "v1"
-    });
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Tobiso API", Version = "v1" });
 
-    options.AddSecurityDefinition("basic", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "basic",
-        In = ParameterLocation.Header,
-        Description = "Enter your username and password for Basic Authentication"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
+    options.AddSecurityDefinition(
+        "basic",
+        new OpenApiSecurityScheme
         {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "basic"
-                }
-            },
-            Array.Empty<string>()
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "basic",
+            In = ParameterLocation.Header,
+            Description = "Enter your username and password for Basic Authentication",
         }
-    });
-});
+    );
 
+    options.AddSecurityRequirement(
+        new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "basic",
+                    },
+                },
+                Array.Empty<string>()
+            },
+        }
+    );
+});
 
 var app = builder.Build();
 
@@ -117,9 +119,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
-else
-{
-}
+else { }
 app.UseSwagger();
 app.UseSwaggerUI();
 
@@ -133,6 +133,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
